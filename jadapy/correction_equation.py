344ad6a77a1ef694a971ff1509d8432742ff6a32
Sven Baars <s.baars@rug.nl>
2021-01-04 11:46:00 +0100

    Properly project the rhs in the correction equation solve.

--- jadapy/correction_equation.py
+++ jadapy/correction_equation.py
@@ -1,6 +1,12 @@
 import numpy
 from scipy.sparse import linalg
 
+def _proj(Q, Y, H, x):
+    y = Q.T.conj() @ x
+    y = numpy.linalg.solve(H, y)
+    y = Y @ y
+    return x - y
+
 class generalized_linear_operator(object):
     def __init__(self, A, B, prec, Q, Y, H, alpha, beta):
         self.A = A
@@ -15,18 +21,16 @@ class generalized_linear_operator(object):
         self.shape = self.A.shape
 
     def matvec(self, x):
-        y = self.Q.T.conj() @ x
-        y = numpy.linalg.solve(self.H, y)
-        y = self.Y @ y
-        y = x - y
+        y = _proj(self.Q, self.Y, self.H, x)
         y = self.beta * (self.A @ y) - self.alpha * (self.B @ y)
         y = self.prec(y)
-        z = self.Q.T.conj() @ y
-        z = numpy.linalg.solve(self.H, z)
-        z = self.Y @ z
-        return y - z
+        return _proj(self.Q, self.Y, self.H, y)
 
 def solve_generalized_correction_equation(A, B, prec, Q, Y, H, alpha, beta, r, tolerance):
     op = generalized_linear_operator(A, B, prec, Q, Y, H, alpha, beta)
+    r = _proj(Q, Y, H, r)
+    r = prec(r)
     v, info = linalg.gmres(op, -r, tol=tolerance, atol=0)
+    if info != 0:
+        raise Exception('GMRES returned ' + str(info))
     return v
