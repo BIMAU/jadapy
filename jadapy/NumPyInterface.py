5c8f0070e90b5cc46e1185895d586cf14e2db5b2
Sven Baars <s.baars@rug.nl>
2021-03-05 09:46:59 +0100

    flake8: Fix some undefined variable warnings.

--- jadapy/NumPyInterface.py
+++ jadapy/NumPyInterface.py
@@ -1,4 +1,5 @@
 import numpy
+import warnings
 
 from scipy.sparse import linalg
 
--- jadapy/jdqz.py
+++ jadapy/jdqz.py
@@ -62,6 +62,7 @@ def jdqz(A, B, num=5, target=Target.SmallestMagnitude, tol=1e-8, prec=None,
 
     alpha = None
     beta = None
+    evs = None
 
     dtype = A.dtype
     ctype = numpy.dtype(dtype.char.upper())
