9f97d1ffa9bc1759b9fb2eeb574ee6b1973d99df
Sven Baars <s.baars@rug.nl>
2021-03-23 13:27:44 +0100

    jdqr: Base the dtype on the interface.

--- jadapy/jdqr.py
+++ jadapy/jdqr.py
@@ -38,8 +38,10 @@ def jdqr(A, num=5, target=Target.SmallestMagnitude, tol=1e-8, M=None, prec=None,
     evs = None
 
     dtype = A.dtype
-    ctype = numpy.dtype(dtype.char.upper())
+    if interface:
+        dtype = interface.dtype
 
+    ctype = numpy.dtype(dtype.char.upper())
     if arithmetic in ['complex', 'c']:
         dtype = ctype
 
