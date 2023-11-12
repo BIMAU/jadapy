c749f5760855fe5273667936824f69b98dcc1b0b
Sven Baars <s.baars@uu.nl>
2023-11-12 11:20:56 +0100

    utils: Fix some deprecation warnings when using the Epetra interface.

--- jadapy/utils.py
+++ jadapy/utils.py
@@ -1,12 +1,18 @@
-from math import sqrt
-
 import numpy
+import math
 
 def dot(x, y):
     try:
         return x.T.conj() @ y
     except AttributeError:
         return x.dot(y)
+
+def sqrt(x):
+    if isinstance(x, numpy.ndarray):
+        assert x.shape[0] == 1 and (len(x.shape) < 2 or x.shape[1] < 2)
+        return math.sqrt(x.item())
+
+    return math.sqrt(x)
 
 def norm(x, M=None):
     def applyM(x):
